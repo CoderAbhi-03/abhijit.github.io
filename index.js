9e341d4dd734af8ae0fb8dcf2cb38e653f699dd9
Abhijit7212 <abhijitghanta@hotmail.com>
2021-12-17 19:42:55 +0530

    abhijit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function show(){
+    var nav=document.getElementById("nav-links");
+    nav.style.right="0";
+}
+function hide(){
+    var nav=document.getElementById("nav-links");
+    nav.style.right="-200px";
+}
